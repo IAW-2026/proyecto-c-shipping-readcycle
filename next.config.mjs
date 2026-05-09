f3456565c704925dc5fd096ce1a7b8b51dc29e8c
Valentino <valennvillar@gmail.com>
2026-05-09 17:11:14 -0300

    prisma setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true, // Usa true para SEO (301) o false para redirección temporal (307)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
