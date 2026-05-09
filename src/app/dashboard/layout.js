bea68aaaee56fb904fa887234271e564aa45b8a7
Valentino <valennvillar@gmail.com>
2026-05-09 18:21:56 -0300

    sign-in implemented

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -1,9 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function DashboardLayout({ children }) {
   return (
     <div className="min-h-full">
       <nav className="flex justify-between p-5 items-center">
         <h1 className="font-bold text-2xl">ACME S.A</h1>
-        <h2 className="">Operador</h2>
+        <UserButton />
       </nav>
       <div className="flex text-4xl justify-center p-5">
         <h2>Portal de Envios</h2>
