88b4d7038bad3a47209ef8c6be1632b6edc4bdde
Valentino <valennvillar@gmail.com>
2026-06-18 21:12:30 -0300

    x-api-key

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -1,7 +1,6 @@
-const API_TOKEN = process.env.API_TOKEN;
-
 export async function checkAPIToken(req) {
   const apiToken = req.headers.get("x-api-token");
+  const API_TOKEN = process.env.API_TOKEN;
 
   if (apiToken || apiToken !== API_TOKEN) {
     throw new Error("Missing or invalid API Token");
